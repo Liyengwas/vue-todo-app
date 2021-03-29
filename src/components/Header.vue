14e1a72e73dacf593b374b1c4e35bc78636d3261
Liyengwas <liyengwas@gmail.com>
2021-03-29 15:27:47 +0300

    Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        props: {
+            title: String
+        }
+    };
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-left: 20px;
+        margin-bottom: 20px;
+    }
+</style>
